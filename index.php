7202bf7cb6131db1c2e22ff5b33d73a028404a53
sejesila <silaseje5@gmail.com>
2021-02-18 18:59:16 +0300

    fronted update

--- index.php
+++ index.php
@@ -46,62 +46,19 @@ include ('includes/header.php')
   </div>
 </section>
 <!-- intro section --> 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-<!-- package section -->
-<section class="video-section">
-  <div class="container"> 
-    <div class="row">  
-            <div id="content24" data-section="content-24" class="data-section"> 
-    		<div class="col-md-6">
-				<h3 class="eidtContent">Content Video</h3>
-				<p class="eidtContent">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-				Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-				when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
-   				<p class="editContent">There are many variations of passages of Lorem Ipsum available,
-   				 but the majority have suffered alteration in some form, by injected humour, 
-   				 or randomised words which don't look even slightly believable.</p>
-				<p class="editContent">There are many variations of passages of Lorem Ipsum available,
-				 but the majority have suffered alteration in some form, by injected humour,
-				  or randomised words which don't look even slightly believable.</p>
-    		</div>
-			<div class="col-md-6">
-				<div class="embed-responsive embed-responsive-16by9">
-				  <iframe class="embed-responsive-item" src="https://player.vimeo.com/video/146742515?badge=0"></iframe>
-				</div>
-			</div> 
-    	</div>
-			</div><!-- end row -->    
-  </div>
-</section>
 <!-- package section --> 
-
-
 <section id="pricing5" data-section="pricing-5" class="data-section">
     <div class="container">
           <div class="section-header">
                 <h2 class="wow fadeInDown animated">Pricing</h2>
-                <p class="wow fadeInDown animated">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent eget risus vitae massa <br> semper aliquam quis mattis quam.</p>
+                <p class="wow fadeInDown animated"> At Eden Dental Clinic we offer a wide range of dental services that are customized to exceed the expectations of every client.</p>
             </div>
-         <div class="row">
-            <div class="col-md-3 col-md-offset-1">
+         
+            <div class="row">
+            <div class="col-md-3">
                 <div class="table">
-                    <h3 class="editContent">Basic</h3>
-                    <h2 class="editContent">$13</h2>
+                    <h3 class="editContent">Dental Consultation</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
                     <p class="editContent">Per Month</p>
                     <ul class="table-content">
                         <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
@@ -114,12 +71,15 @@ include ('includes/header.php')
                         <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
                     </div>
                 </div>
-            </div>
 
-            <div class="col-md-4">
-                <div class="table long-table">
-                    <h3 class="editContent">Premium</h3>
-                    <h2 class="editContent">$23</h2>
+            </div>
+         
+         
+         <div class="col-md-3">
+         	<div class="col-md-3"></div>
+                <div class="table">
+                    <h3 class="editContent">Tooth Extraction</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
                     <p class="editContent">Per Month</p>
                     <ul class="table-content">
                         <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
@@ -129,16 +89,36 @@ include ('includes/header.php')
                     </ul>
 
                     <div class="text-center text-uppercase">
-                        <a href="#" class="btn btn-default-blue-tiny editContent">Signup Now</a>
+                        <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
+                    </div>
+                </div>
+                
+
+            </div>
+         
+         <div class="col-md-3">
+                <div class="table">
+                    <h3 class="editContent">Tooth Filling</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
+                    <p class="editContent">Per Month</p>
+                    <ul class="table-content">
+                        <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
+                        <li class="editContent"><i class="fa fa-pie-chart"></i> 500 GB Bandwidth</li>
+                        <li class="editContent"><i class="fa fa-envelope-o"></i> Email Support</li>
+                        <li class="editContent"><i class="fa fa-cogs"></i> 24x7 Support</li>
+                    </ul>
+
+                    <div class="text-center text-uppercase">
+                        <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
                     </div>
                 </div>
 
             </div>
-
-            <div class="col-md-3">
+         
+         <div class="col-md-3">
                 <div class="table">
-                    <h3 class="editContent">Developer</h3>
-                    <h2 class="editContent">$33</h2>
+                    <h3 class="editContent">Root Canal Treatment, Operculectomy</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
                     <p class="editContent">Per Month</p>
                     <ul class="table-content">
                         <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
@@ -154,7 +134,89 @@ include ('includes/header.php')
 
             </div>
          </div>
-    </div>
+            // <div class="row">
+            <div class="col-md-3">
+                <div class="table">
+                    <h3 class="editContent">Third Molar Surgery,</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
+                    <p class="editContent">Per Month</p>
+                    <ul class="table-content">
+                        <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
+                        <li class="editContent"><i class="fa fa-pie-chart"></i> 500 GB Bandwidth</li>
+                        <li class="editContent"><i class="fa fa-envelope-o"></i> Email Support</li>
+                        <li class="editContent"><i class="fa fa-cogs"></i> 24x7 Support</li>
+                    </ul>
+
+                    <div class="text-center text-uppercase">
+                        <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
+                    </div>
+                </div>
+
+            </div>
+         
+         
+         <div class="col-md-3">
+         	<div class="col-md-3"></div>
+                <div class="table">
+                    <h3 class="editContent">Teeth Whitening</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
+                    <p class="editContent">Per Month</p>
+                    <ul class="table-content">
+                        <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
+                        <li class="editContent"><i class="fa fa-pie-chart"></i> 500 GB Bandwidth</li>
+                        <li class="editContent"><i class="fa fa-envelope-o"></i> Email Support</li>
+                        <li class="editContent"><i class="fa fa-cogs"></i> 24x7 Support</li>
+                    </ul>
+
+                    <div class="text-center text-uppercase">
+                        <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
+                    </div>
+                </div>
+                
+
+            </div>
+         
+         <div class="col-md-3">
+                <div class="table">
+                    <h3 class="editContent">Teeth Alignment(Braces), Uvulectomy</h3>
+                    <h2 class="editContent">Ksh. 00</h2>
+                    <p class="editContent">Per Month</p>
+                    <ul class="table-content">
+                        <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
+                        <li class="editContent"><i class="fa fa-pie-chart"></i> 500 GB Bandwidth</li>
+                        <li class="editContent"><i class="fa fa-envelope-o"></i> Email Support</li>
+                        <li class="editContent"><i class="fa fa-cogs"></i> 24x7 Support</li>
+                    </ul>
+
+                    <div class="text-center text-uppercase">
+                        <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
+                    </div>
+                </div>
+
+            </div>
+         
+         <div class="col-md-3">
+                <div class="table">
+                    <h3 class="editContent">Full Mouth Scaling</h3>
+                    <h2 class="editContent">Ksh. 000</h2>
+                    <p class="editContent">per specific</p>
+                    <ul class="table-content">
+                        <li class="editContent"><i class="fa fa-hdd-o"></i> 10 GB Storage</li>
+                        <li class="editContent"><i class="fa fa-pie-chart"></i> 500 GB Bandwidth</li>
+                        <li class="editContent"><i class="fa fa-envelope-o"></i> Email Support</li>
+                        <li class="editContent"><i class="fa fa-cogs"></i> 24x7 Support</li>
+                    </ul>
+
+                    <div class="text-center text-uppercase">
+                        <a href="#" class="btn btn-default-green-transparent-tiny editContent">Signup Now</a>
+                    </div>
+                </div>
+
+            </div>
+         </div>
+         
+         </div>
+
 </section>
 
 
